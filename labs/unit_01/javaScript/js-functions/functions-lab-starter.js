cbaaa8a29e082327322cebdf3119df5ab320d57e
burnsbeaver <burnsbeaver@gmail.com>
2017-07-06 17:32:45 -0400

    Completed Timer Countdown

--- labs/unit_01/javaScript/js-functions/functions-lab-starter.js
+++ labs/unit_01/javaScript/js-functions/functions-lab-starter.js
@@ -1,53 +1,93 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(number1, number2) {
+  if (number1 > number2) {
+    return number1
+  }
+  if else (number2 > number1){
+    return number2
+  }
+  else {return "Equal"}
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(number1, number2, number3) {
+  if (number1 > number2 && number1 > number3) {
+    return number1
+  }
+  else if (number2 > number1 && number2 > number3){
+    return number2
+  }
+  else {return "Equal"}
 }
+
 
 // Question 3
-function isCharacterAVowel() {
-
+function isCharacterAVowel(str) {
+  var letter = str.toString
+  if (letter.toUpperCase() === "A" || letter.toUpperCase() ==="E" || letter.toUpperCase() === "I" || letter.toUpperCase() === "O" || letter.toUpperCase() === "U") {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(numbers) {
+  var arraySum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    arraySum += numbers[i];
+  }
+  return arraySum
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(numbers) {
+  var arrayTotal = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    arrayTotal = arrayTotal * numbers[i];
+  }
+  return arrayTotal;
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+  return args.length;
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
+var reverseString = function (str){
+  return str.split('').reverse().join('')
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+function findLongestWord (arrayOfWords) {
+  var longestWord = '';
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    if (arrayOfWords[i].length > longestWord.length) {
+      longestWord = arrayOfWords[i];
+    }
+  }
+  return longestWord
 }
 
 
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords (arrayOfWords, wordLength) {
+  var longWords = []
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    if (arrayOfWords[i].length > wordLength) {
+      longWords.push(arrayOfWords[i])
+    }
+  }
+  return longWords
 }
+
 
 
 // Bonus 1
@@ -56,5 +96,5 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
+
 }
