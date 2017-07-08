3185b6855a12ddfa23f8333aa3eb123b8fe8884b
burnsbeaver <burnsbeaver@gmail.com>
2017-07-08 14:20:10 -0400

    Got counters to appear

--- homework/unit_01/javaScript/counter-factory/hw.js
+++ homework/unit_01/javaScript/counter-factory/hw.js
@@ -49,7 +49,13 @@ const CounterCollection = {
 const Presenter = {
   insertCounterComponent: function(newCountId){
     console.log(`insert counter component #${newCountId}`);
-    // Your Code Here
+    let newCounterComponent = document.createElement('div');
+    newCounterComponent.innerHTML =
+    `<h3>Count: <span>0</span></h3> <button class='increment'> +1 </button> <button class='delete'> Delete </button>`;
+    newCounterComponent.className += ' counter';
+    newCounterComponent.dataset.countId = newCountId;
+    newCounterComponent.getElementsByClassName('increment')[0].onclick= AppController.onClickIncrement;
+    document.getElementById('counter-list').appendChild(newCounterComponent);
   },
   refreshCounterComponent: function(countId){
     console.log(`refresh counter component #${countId}`);
@@ -64,7 +70,10 @@ const Presenter = {
 // Top-Level Application Control //
 const AppController = {
   onClickNewCounter: function(event){
-    // Your Code Here
+    CounterCollection.createCounter();
+    Presenter.insertCounterComponent(CounterCollection.lastCountId)
+    console.log(`click new counter (#${CounterCollection.lastCountId})`);
+
   },
   onClickIncrement: function(event){
     // Your Code Here
