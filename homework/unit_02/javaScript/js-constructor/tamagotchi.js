4126f93a959c859cb507e16c9ef43aadae893119
burnsbeaver <burnsbeaver@gmail.com>
2017-07-24 18:29:56 -0400

    part 3: added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -14,6 +14,14 @@ class Tamagotchis {
     console.log(this.foodInTummy);
     console.log(this.name + ' is crying. WAAAAHHHHH!!!!!')
   }
+  puke() {
+    this.foodInTummy -= 1;
+    console.log(this.foodInTummy + "BLAHHHHT")
+  }
+  yawn() {
+    this.restedness -= 1;
+    console.log(this.name + " has current restedness of: " + this.restedness)
+  }
 }
 
 //create new Tamagotchis
@@ -21,5 +29,7 @@ var burnsTamagochi = new Tamagotchis("Burns", "fire");
 var quinnTamagochi = new Tamagotchis("Quinn", "water");
 
 //test out your Tamagotchies below via console.logs
-burnsTamagochi.cry();
-quinnTamagochi.cry();
+burnsTamagochi.puke()
+quinnTamagochi.yawn()
+console.log(burnsTamagochi)
+console.log(quinnTamagochi)
