218676aaf2c0723165f13e9a3f2d2cbc6eb8d2b0
burnsbeaver <burnsbeaver@gmail.com>
2017-07-24 20:35:34 -0400

    Finished level 2

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -1,3 +1,16 @@
-setTimeout(function(){
-  console.log('TIMED OUT!')
-}, 300);
+require('es6-promise');
+'use strict'
+// setTimeout(function(){
+//   console.log('TIMED OUT!');
+// }, 300);
+
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(function(){
+    fulfill('FULFILLED!');
+  }, 300)
+});
+
+promise.then(console.log)
+// promise.then((success){
+//   console.log('FULFILLED!');
+// });
