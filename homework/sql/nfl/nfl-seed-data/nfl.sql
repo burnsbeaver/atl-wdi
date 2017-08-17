b93cef6179a50610204ef47f398076d712ccb9f4
burnsbeaver <burnsbeaver@gmail.com>
2017-08-17 18:43:49 -0400

    Tons of nfl data

---
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -0,0 +1,15 @@
+step 3
+
+  1. Select name from Teams;
+
+  2.SELECT stadium, head_coach FROM Teams
+
+  3. select name, head_coach from teams where division = 'South' AND conference = 'AFC';
+
+  4.     SELECT COUNT(*) FROM players;
+
+  5. select name, head_coach from teams where division = 'North' AND conference = 'NFC' OR division = 'East' AND conference = 'AFC';
+
+  6. select AVG(salary) from players;
+
+  7.  SELECT name, position FROM players WHERE salary > 10000000;
