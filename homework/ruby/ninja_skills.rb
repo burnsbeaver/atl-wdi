e56e054369287197897029c6571a82383a7733a0
burnsbeaver <burnsbeaver@gmail.com>
2017-08-21 19:44:12 -0400

    4. string skillzzzz

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -12,9 +12,18 @@
 # puts var_nil.class
 
 # Challenge 3
-d = 20
-puts d
-e = 3.2
-puts e
-d = e
-puts d
+# d = 20
+# puts d
+# e = 3.2
+# puts e
+# d = e
+# puts d
+
+# Challege 4
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda."
+tims = "and I like"
+puts "#{christine} #{tims} #{likesTo}"
+g = "2"
+gToNumber = 2
+puts g.to_i + gToNumber
