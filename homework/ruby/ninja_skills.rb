f20a89f269ce824be195138f673e8d198f3ddc70
burnsbeaver <burnsbeaver@gmail.com>
2017-08-21 19:40:25 -0400

    Integer Skillzzz

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,12 +1,20 @@
-# Challenge 1
-a = true
-b = false
-puts a && b
-puts a || b
+# # Challenge 1
+# a = true
+# b = false
+# puts a && b
+# puts a || b
+#
+# # Challenge 2
+# var_nil = nil
+# puts var_nil
+# puts "#{var_nil} was here"
+# print var_nil
+# puts var_nil.class
 
-# Challenge 2
-var_nil = nil
-puts var_nil
-puts "#{var_nil} was here"
-print var_nil
-puts var_nil.class
+# Challenge 3
+d = 20
+puts d
+e = 3.2
+puts e
+d = e
+puts d
